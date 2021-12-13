38f255bb833543fc6768fddeebc3aabcaeab7757
Luis Pozo <lpozo94@gmail.com>
2021-12-13 15:00:07 -0500

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-//all exports components will go here!!!
-export { default as Navbar } from './Navbar';
-export { default as Homepage } from './Homepage';
-export { default as Exchanges } from './Exchanges';
-export { default as Cryptocurrencies } from './Cryptocurrencies';
-export { default as CryptoDetails } from './CryptoDetails';
-export { default as News } from './News';
